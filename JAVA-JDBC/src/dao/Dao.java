5aaf1739190a0b5bd2524c3715fad8e1e9dc904e
root101 <root101@LAPTOP-J0ECD42D.lan>
2022-02-05 09:47:43 +0100

    no ssl update

--- JAVA-JDBC/src/dao/Dao.java
+++ JAVA-JDBC/src/dao/Dao.java
@@ -19,7 +19,7 @@ public class Dao {
 	public static final String CONNECTION = 
 			"jdbc:mysql://localhost:3306/" + 
 			SCHEMA_NAME + 
-			"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
 	public static final String USER_CONNECTION = "nombreUsuario";
 	public static final String PASS_CONNECTION = "contraseña";
 	
